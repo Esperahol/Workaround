c2c07f3411aa49de74c0bb97aead4b8527c5820a
Esperahol <esperahol@gmail.com>
2022-11-12 16:26:34 -0500

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,163 @@
+export const COMMENTS =
+    [
+        {
+            id: 0,
+            beerId: 0,
+            rating: 1,
+            comment: "I've had stronger dish water.",
+            author: "Lemon Jon",
+            date: "2022-10-16T17:57:28.556094Z"
+        },
+        {
+            id: 1,
+            beerId: 0,
+            rating: 4,
+            comment: "I actually forgot my name for a moment - 10/10 would again.",
+            author: "Paul McVites",
+            date: "2014-09-05T17:57:28.556094Z"
+        },
+        {
+            id: 2,
+            beerId: 0,
+            rating: 3,
+            comment: "It gets the job done.",
+            author: "Michael Jaikishan",
+            date: "2015-02-13T17:57:28.556094Z"
+        },
+        {
+            id: 3,
+            beerId: 0,
+            rating: 4,
+            comment: "If only it weren't so expensive.",
+            author: "Ringo Starry",
+            date: "2013-12-02T17:57:28.556094Z"
+        },
+        {
+            id: 4,
+            beerId: 0,
+            rating: 2,
+            comment: "Partying on a budget.",
+            author: "25 Cent",
+            date: "2011-12-02T17:57:28.556094Z"
+        },
+        {
+            id: 5,
+            beerId: 1,
+            rating: 5,
+            comment: "I have met the love of my life - too bad I'm broke.",
+            author: "John Lemon",
+            date: "2012-10-16T17:57:28.556094Z"
+        },
+        {
+            id: 6,
+            beerId: 1,
+            rating: 4,
+            comment: "I haven't had anything like it before.",
+            author: "Paul McVites",
+            date: "2014-09-05T17:57:28.556094Z"
+        },
+        {
+            id: 7,
+            beerId: 1,
+            rating: 3,
+            comment: "Good for chugging - not much for enjoying.",
+            author: "Michael Jaikishan",
+            date: "2015-02-13T17:57:28.556094Z"
+        },
+        {
+            id: 8,
+            beerId: 1,
+            rating: 4,
+            comment: "Best I've had yet - but the price tag is worthless.",
+            author: "Ringo Starry",
+            date: "2013-12-02T17:57:28.556094Z"
+        },
+        {
+            id: 9,
+            beerId: 1,
+            rating: 2,
+            comment: "It's your birthday, we're gonna party!",
+            author: "25 Cent",
+            date: "2011-12-02T17:57:28.556094Z"
+        },
+        {
+            id: 10,
+            beerId: 2,
+            rating: 5,
+            comment: "Imagine all the eatables, living in conFusion!",
+            author: "John Lemon",
+            date: "2012-10-16T17:57:28.556094Z"
+        },
+        {
+            id: 11,
+            beerId: 2,
+            rating: 4,
+            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+            author: "Paul McVites",
+            date: "2014-09-05T17:57:28.556094Z"
+        },
+        {
+            id: 12,
+            beerId: 2,
+            rating: 3,
+            comment: "Eat it, just eat it!",
+            author: "Michael Jaikishan",
+            date: "2015-02-13T17:57:28.556094Z"
+        },
+        {
+            id: 13,
+            beerId: 2,
+            rating: 4,
+            comment: "Ultimate, Reaching for the stars!",
+            author: "Ringo Starry",
+            date: "2013-12-02T17:57:28.556094Z"
+        },
+        {
+            id: 14,
+            beerId: 2,
+            rating: 2,
+            comment: "It's your birthday, we're gonna party!",
+            author: "25 Cent",
+            date: "2011-12-02T17:57:28.556094Z"
+        },
+        {
+            id: 15,
+            beerId: 3,
+            rating: 5,
+            comment: "Imagine all the eatables, living in conFusion!",
+            author: "John Lemon",
+            date: "2012-10-16T17:57:28.556094Z"
+        },
+        {
+            id: 16,
+            beerId: 3,
+            rating: 4,
+            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+            author: "Paul McVites",
+            date: "2014-09-05T17:57:28.556094Z"
+        },
+        {
+            id: 17,
+            beerId: 3,
+            rating: 3,
+            comment: "Eat it, just eat it!",
+            author: "Michael Jaikishan",
+            date: "2015-02-13T17:57:28.556094Z"
+        },
+        {
+            id: 18,
+            beerId: 3,
+            rating: 4,
+            comment: "Ultimate, Reaching for the stars!",
+            author: "Ringo Starry",
+            date: "2013-12-02T17:57:28.556094Z"
+        },
+        {
+            id: 19,
+            beerId: 3,
+            rating: 2,
+            comment: "It's your birthday, we're gonna party!",
+            author: "25 Cent",
+            date: "2011-12-02T17:57:28.556094Z"
+        }
+    ]
